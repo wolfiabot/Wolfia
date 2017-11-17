df23cac080f5a3eba0ad81834a5d4f8df6257ecc
Napster <napster@npstr.space>
2017-11-17 20:17:29 +0100

    Remove cron script, improve update script

--- cron.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-cd /home/napster/wolfia
-sh ./docker-update.sh
--- docker-update.sh
+++ docker-update.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# this can be set up as a (daily) cron job like this:
+# 0  9  *   *   *     cd /home/napster/wolfia && ./docker-update.sh &>> logs/cron.log
+
+echo $(date)
 docker-compose pull
 docker-compose up -d --timeout 7300 --remove-orphans #make sure to adjust this to the value in the shutdown hook
 docker image prune --force
