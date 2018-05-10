f318573d561b6346e87684d5cd33914b2fb95fe6
Napster <napster@npstr.space>
2018-05-10 23:07:17 +0200

    Refactor: Run as a spring boot app with Launcher

---
+++ src/main/java/space/npstr/wolfia/Launcher.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2017-2018 Dennis Neufeld
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package space.npstr.wolfia;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.context.event.ApplicationFailedEvent;
+
+/**
+ * Created by napster on 10.05.18.
+ */
+@SpringBootApplication(exclude = { //we handle these ourselves
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class,
+        FlywayAutoConfiguration.class
+})
+public class Launcher implements ApplicationRunner {
+
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(Launcher.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Launcher.class);
+        app.addListeners(event -> {
+            if (event instanceof ApplicationFailedEvent) {
+                ApplicationFailedEvent failed = (ApplicationFailedEvent) event;
+                log.error("Application failed", failed.getException());
+            }
+        });
+        app.run(args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Wolfia.main(args.getSourceArgs());
+    }
+}
