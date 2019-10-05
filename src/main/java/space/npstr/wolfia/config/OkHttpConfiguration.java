a1d042677d0e53b02736de40bcb44c886d4b628c
Napster <napster@npstr.space>
2019-10-05 12:52:04 +0200

    Update Copyright header
    2016 first publication, 2019 latest modifications to the project

--- src/main/java/space/npstr/wolfia/config/OkHttpConfiguration.java
+++ src/main/java/space/npstr/wolfia/config/OkHttpConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017-2019 Dennis Neufeld
+ * Copyright (C) 2016-2019 Dennis Neufeld
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
