6d84a41512ac569298f5e38c8885c455588be841
Napster <napster@npstr.space>
2020-03-09 00:59:59 +0100

    Set up .editorconfig, ESLint and Prettier

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -1,55 +1,58 @@
 <template>
-		<div id="app">
-			<div id="nav">
-				<router-link to="/">Home</router-link> |
-				<router-link to="/about">About</router-link>
-			</div>
-			<router-view/>
+	<div id="app">
+		<div id="nav">
+			<router-link to="/">Home</router-link> |
+			<router-link to="/about">About</router-link>
 		</div>
+		<router-view />
+	</div>
 </template>
 
 <script>
-	export default {
-		mounted() {
-			//userreport snippet
-			window._urq = window._urq || [];
-			window._urq.push(['initSite', '01987d31-0d58-48c6-a4d3-96f2ae42eb14']);
-			(function () {
-				const ur = document.createElement('script');
-				ur.type = 'text/javascript';
-				ur.async = true;
-				ur.src = ('https:' === document.location.protocol ? 'https://cdn.userreport.com/userreport.js' : 'http://cdn.userreport.com/userreport.js');
-				const s = document.getElementsByTagName('script')[0];
-				s.parentNode.insertBefore(ur, s);
-			})();
-		}
+export default {
+	mounted() {
+		// userreport snippet
+		window._urq = window._urq || [];
+		window._urq.push(["initSite", "01987d31-0d58-48c6-a4d3-96f2ae42eb14"]);
+		(function() {
+			const ur = document.createElement("script");
+			ur.type = "text/javascript";
+			ur.async = true;
+			ur.src =
+				document.location.protocol === "https:"
+					? "https://cdn.userreport.com/userreport.js"
+					: "http://cdn.userreport.com/userreport.js";
+			const s = document.getElementsByTagName("script")[0];
+			s.parentNode.insertBefore(ur, s);
+		})();
 	}
+};
 </script>
 
 <style>
-	body {
-		background-color: #001830;
-		height: 100vh;
-        color: beige;
-	}
+body {
+	background-color: #001830;
+	height: 100vh;
+	color: beige;
+}
 
-	#app {
-		font-family: 'Avenir', Helvetica, Arial, sans-serif;
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
-		text-align: center;
-	}
+#app {
+	font-family: "Avenir", Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+}
 
-	#nav {
-		padding: 30px;
-	}
+#nav {
+	padding: 30px;
+}
 
-	#nav a {
-		font-weight: bold;
-        color: beige;
-	}
+#nav a {
+	font-weight: bold;
+	color: beige;
+}
 
-	#nav a.router-link-exact-active {
-		color: khaki;
-	}
+#nav a.router-link-exact-active {
+	color: khaki;
+}
 </style>
--- dashboard/src/views/Home.vue
+++ dashboard/src/views/Home.vue
@@ -1,48 +1,48 @@
 <template>
-  <div class="home">
-    <img v-if="loaded" alt="User logo" :src="avatarUrl">
-    <img v-else alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App" :user="user"/>
-  </div>
+	<div class="home">
+		<img v-if="loaded" alt="User logo" :src="avatarUrl" />
+		<img v-else alt="Vue logo" src="../assets/logo.png" />
+		<HelloWorld msg="Welcome to Your Vue.js App" :user="user" />
+	</div>
 </template>
 
 <script>
-  // @ is an alias to /src
-  import HelloWorld from '@/components/HelloWorld.vue'
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
 
-  export default {
-    name: 'home',
-    components: {
-      HelloWorld
-    },
-    data() {
-      return {
-        loaded: false,
-        user: {
-          discordId: 42,
-          name: 'mysterious person',
-          avatarId: 42,
-        },
-      }
-    },
+export default {
+	name: "home",
+	components: {
+		HelloWorld
+	},
+	data() {
+		return {
+			loaded: false,
+			user: {
+				discordId: 42,
+				name: "mysterious person",
+				avatarId: 42
+			}
+		};
+	},
 
-    mounted() {
-      this.getUser();
-    },
+	mounted() {
+		this.getUser();
+	},
 
-    computed: {
-      avatarUrl() {
-        const ext = this.user.avatarId.startsWith("a_") ? "gif" : "png";
-        return `https://cdn.discordapp.com/avatars/${this.user.discordId}/${this.user.avatarId}.${ext}`;
-      },
-    },
+	computed: {
+		avatarUrl() {
+			const ext = this.user.avatarId.startsWith("a_") ? "gif" : "png";
+			return `https://cdn.discordapp.com/avatars/${this.user.discordId}/${this.user.avatarId}.${ext}`;
+		}
+	},
 
-    methods: {
-      async getUser() {
-        const response = await fetch('/api/user');
-        this.user = await response.json();
-        this.loaded = true;
-      },
-    },
-  }
+	methods: {
+		async getUser() {
+			const response = await fetch("/api/user");
+			this.user = await response.json();
+			this.loaded = true;
+		}
+	}
+};
 </script>
