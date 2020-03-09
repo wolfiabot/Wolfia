3ee8995af1835156d7c94a3f6480d48396884f5c
Napster <napster@npstr.space>
2020-03-09 14:54:09 +0100

    Enable VueX strict mode in dev

--- dashboard/src/store/store.js
+++ dashboard/src/store/store.js
@@ -4,6 +4,7 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+	strict: process.env.NODE_ENV !== "production", //see https://vuex.vuejs.org/guide/strict.html
 	state: {},
 	mutations: {},
 	actions: {},
