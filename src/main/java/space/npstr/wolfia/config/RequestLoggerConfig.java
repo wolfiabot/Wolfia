40deda45f5b401fe14347b4518d4925294878741
Napster <napster@npstr.space>
2020-03-12 00:53:23 +0100

    Add request debug logger

--- src/main/java/space/npstr/wolfia/config/RequestLoggerConfig.java
+++ src/main/java/space/npstr/wolfia/config/RequestLoggerConfig.java
@@ -1,4 +1,41 @@
+/*
+ * Copyright (C) 2016-2020 Dennis Neufeld
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package space.npstr.wolfia.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+/**
+ * Turn on with {@code org.springframework.web.filter.CommonsRequestLoggingFilter: DEBUG}
+ */
+@Configuration
 public class RequestLoggerConfig {
+
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setIncludeHeaders(true);
+        filter.setIncludeClientInfo(true);
+        return filter;
+    }
+
 }
