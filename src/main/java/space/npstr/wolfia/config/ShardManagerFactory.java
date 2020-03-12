81b4bc1165c94a3cdeed2952dbc9db14bb1f2090
Napster <napster@npstr.space>
2020-03-12 18:09:28 +0100

    Bump JDA & implement Gateway Intents

--- src/main/java/space/npstr/wolfia/config/ShardManagerFactory.java
+++ src/main/java/space/npstr/wolfia/config/ShardManagerFactory.java
@@ -17,7 +17,13 @@
 
 package space.npstr.wolfia.config;
 
+import java.util.EnumSet;
+import java.util.List;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.function.Supplier;
+import javax.security.auth.login.LoginException;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
 import net.dv8tion.jda.api.sharding.ShardManager;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
@@ -30,10 +36,14 @@ import space.npstr.wolfia.config.properties.WolfiaConfig;
 import space.npstr.wolfia.system.DiscordEventListenerPublisher;
 import space.npstr.wolfia.utils.Memoizer;
 
-import javax.security.auth.login.LoginException;
-import java.util.EnumSet;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.function.Supplier;
+import static net.dv8tion.jda.api.requests.GatewayIntent.DIRECT_MESSAGES;
+import static net.dv8tion.jda.api.requests.GatewayIntent.DIRECT_MESSAGE_REACTIONS;
+import static net.dv8tion.jda.api.requests.GatewayIntent.DIRECT_MESSAGE_TYPING;
+import static net.dv8tion.jda.api.requests.GatewayIntent.GUILD_EMOJIS;
+import static net.dv8tion.jda.api.requests.GatewayIntent.GUILD_MEMBERS;
+import static net.dv8tion.jda.api.requests.GatewayIntent.GUILD_MESSAGES;
+import static net.dv8tion.jda.api.requests.GatewayIntent.GUILD_MESSAGE_REACTIONS;
+import static net.dv8tion.jda.api.requests.GatewayIntent.GUILD_MESSAGE_TYPING;
 
 /**
  * We need a factory around the ShardManager bean to proxy its dependencies, so we can have more realistic
@@ -42,6 +52,18 @@ import java.util.function.Supplier;
  */
 @Component
 public class ShardManagerFactory {
+
+    private static final List<GatewayIntent> GATEWAY_INTENTS = List.of(
+            //GUILDS, not supported to be turned off by JDA, listed for documentation.
+            GUILD_EMOJIS, // we use some custom ones, and usage will likely grow bigger in the future, so its a good idea to stay up to date with these
+            GUILD_MESSAGES, // process messages in guilds
+            DIRECT_MESSAGES, // we have some in-game roles that send their commands in DMs
+            GUILD_MESSAGE_REACTIONS, // some reactions are used, for example when voting in the wolf chat
+            DIRECT_MESSAGE_REACTIONS, // some in-game roles may also get reaction based selections in their DMs
+            GUILD_MESSAGE_TYPING, DIRECT_MESSAGE_TYPING, // we use these to determine inactive users and remove them from our game queues
+            // priviledged
+            GUILD_MEMBERS // Required for our management of the wolfchat guilds
+    );
 
     private final WolfiaConfig wolfiaConfig;
     private final DiscordEventListenerPublisher discordEventListenerPublisher;
@@ -74,7 +96,7 @@ public class ShardManagerFactory {
     }
 
     private synchronized ShardManager createShardManager() {
-        DefaultShardManagerBuilder builder = new DefaultShardManagerBuilder()
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.create(GATEWAY_INTENTS)
                 .setToken(this.wolfiaConfig.getDiscordToken())
                 .setActivity(Activity.playing(App.GAME_STATUS))
                 .addEventListeners(this.discordEventListenerPublisher)
