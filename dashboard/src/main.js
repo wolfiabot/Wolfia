161c42064a9a8e91a3b51d3e71e09562e642c9a8
Napster <napster@npstr.space>
2020-03-12 17:03:43 +0100

    Configure Sentry on the frontend

--- dashboard/src/main.js
+++ dashboard/src/main.js
@@ -2,6 +2,22 @@ import Vue from "vue";
 import App from "@/App.vue";
 import router from "@/router/router";
 import store from "@/store/store";
+import * as Sentry from "@sentry/browser";
+import * as Integrations from "@sentry/integrations";
+
+if (process.env.NODE_ENV === "production") {
+	Sentry.init({
+		dsn: process.env.VUE_APP_SENTRY_DSN,
+		integrations: [
+			new Integrations.Vue({
+				Vue,
+				attachProps: true,
+				logErrors: true
+			})
+		],
+		release: process.env.VUE_APP_VERSION
+	});
+}
 
 Vue.config.productionTip = false;
 
