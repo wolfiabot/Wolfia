b9fdcaf525f4f53a70d7e3f7c5456debf19e2c41
Napster <napster@npstr.space>
2020-04-18 20:11:36 +0200

    Log shard status changes

---
+++ src/main/java/space/npstr/wolfia/system/ShardStatusLogger.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2016-2020 Dennis Neufeld
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package space.npstr.wolfia.system;
+
+import net.dv8tion.jda.api.events.StatusChangeEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ShardStatusLogger {
+
+    private static final Logger log = LoggerFactory.getLogger(ShardStatusLogger.class);
+
+    @EventListener(StatusChangeEvent.class)
+    public void onStatusChange(StatusChangeEvent event) {
+        log.info("{} {}", event.getEntity().getShardInfo(), event);
+    }
+}
