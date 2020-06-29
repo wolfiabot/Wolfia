f842c2619487221b27c60b8de7f4226f5cb24e97
OtterBoops <me44888@gmail.com>
2020-06-29 14:02:28 +0300

    Move header to its own component

---
+++ dashboard/src/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  		<nav id="nav" class="navbar has-text-weight-bold">
+			<div class="navbar-brand">
+				<router-link class="navbar-item" to="/">
+					<img src="../assets/logo.png" alt="Play Werewolf/Mafia in Discord" />
+				</router-link>
+				<a role="button" class="navbar-burger" data-target="navMenu" aria-label="menu" aria-expanded="false">
+					<span aria-hidden="true"></span>
+					<span aria-hidden="true"></span>
+					<span aria-hidden="true"></span>
+				</a>
+			</div>
+			<div class="navbar-menu" id="navMenu">
+				<div class="navbar-start">
+					<router-link to="/" class="navbar-item">
+						Home
+					</router-link>
+					<hr class="navbar-divider" />
+					<router-link to="/about" class="navbar-item">
+						About
+					</router-link>
+					<router-link to="/team" class="navbar-item">
+						Team
+					</router-link>
+					<router-link to="/dashboard" class="navbar-item">
+						Dashboard
+					</router-link>
+				</div>
+				<div class="navbar-end">
+					<UserNav></UserNav>
+				</div>
+			</div>
+		</nav>
+</template>
+
+<script>
+import UserNav from "@/components/user/UserNav";
+
+export default {
+    components: {
+        UserNav,
+    }
+
+}
+</script>
+
+<style>
+
+</style>
