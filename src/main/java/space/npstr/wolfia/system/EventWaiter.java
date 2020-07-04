492e6a6052e2ec19df936aaf3be0e870207bf9ea
Napster <napster@npstr.space>
2020-07-04 19:14:43 +0200

    Update License headers
    added missing ones
    took the spring project as an example for an inclusive wording
    also updated the license files (HTTPS doesn't for old licenses)
    
    closes #109

--- src/main/java/space/npstr/wolfia/system/EventWaiter.java
+++ src/main/java/space/npstr/wolfia/system/EventWaiter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Dennis Neufeld
+ * Copyright (C) 2016-2020 the original author or authors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
