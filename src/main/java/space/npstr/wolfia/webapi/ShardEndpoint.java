a0f1562b0e37cd4b97a8d48bd8e8d43599d7ce89
OtterBoops <me44888@gmail.com>
2020-07-09 13:59:56 +0300

    Implement public shards endpoint

---
+++ src/main/java/space/npstr/wolfia/webapi/ShardEndpoint.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2016-2020 the original author or authors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package space.npstr.wolfia.webapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import net.minidev.json.JSONObject;
+import space.npstr.wolfia.domain.stats.ShardService;
+
+@RestController
+@RequestMapping("/public/shards")
+public class ShardEndpoint {
+
+    private final ShardService shardService;
+
+    public ShardEndpoint(ShardService shardService) {
+        this.shardService = shardService;
+    }
+
+    @GetMapping
+    public JSONObject getShards() {
+        return this.shardService.getShardStatus();
+    }
+}
