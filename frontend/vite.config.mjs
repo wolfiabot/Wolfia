98032a8facc5f29a47e86a7752781e847e9e1a7f
Napster <napster@npstr.space>
2024-01-14 13:29:15 +0100

    Bump frontend dependencies

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -17,7 +17,7 @@
 import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-const path = require("path");
+const path = await import("path");
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
