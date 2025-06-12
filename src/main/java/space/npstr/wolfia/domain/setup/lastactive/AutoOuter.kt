c18e78f0b33467d9fb184ea9bb9b91ec49146094
Schnapster <napster@npstr.space>
2025-06-12 22:32:42 +0200

    2025 Copyright

--- src/main/java/space/npstr/wolfia/domain/setup/lastactive/AutoOuter.kt
+++ src/main/java/space/npstr/wolfia/domain/setup/lastactive/AutoOuter.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016-2024 the original author or authors
+ * Copyright (C) 2016-2025 the original author or authors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
